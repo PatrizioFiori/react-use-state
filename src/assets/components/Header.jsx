86cb7ff767f666b83b58647448f8b4826c20479d
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-01-16 15:47:30 +0100

    Lavoro sul main

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='bg-light'>
+            <h1>Learn Web Development</h1>
+        </header>
+    )
+}
+
+export default Header
