227a4fce3da32ec08bd0155017b0867b21429678
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-01-16 17:09:36 +0100

    Def

--- src/assets/components/Main.jsx
+++ src/assets/components/Main.jsx
@@ -3,21 +3,22 @@ import { useState } from "react"
 
 const Main = () => {
 
-    const { lang, setLang } = useState
+    const [lang, setLang] = useState(0)
+    const [color, setcolor] = useState(0)
 
     return (
 
         <div className="container">
             <div className="sezioneBtn text-center">
-                {languages.map((linguaggi) => {
+                {languages.map((linguaggi, index) => {
                     return (
-                        <button className="mx-5 p-2 rounded-3" key={linguaggi.id} >{linguaggi.title}</button>
+                        <button onClick={() => { setLang(index) }} className="mx-5 p-2 rounded-3" key={linguaggi.id} >{linguaggi.title}</button>
                     )
                 })}
             </div>
             <div className="container sezioneTesto">
-                <h2 className="p-3">w</h2>
-                <p className="p-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum consequuntur vel ipsum optio laboriosam deserunt, fugiat deleniti? Aut sit culpa cum ea fugiat, sed placeat libero earum recusandae repudiandae voluptatem.</p>
+                <h2 className="p-3">{languages[lang].title}</h2>
+                <p className="p-3">{languages[lang].description}</p>
 
             </div>
         </div>
