8743d055f93afaf0b203ab44cd02de526df709bb
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-01-16 16:06:12 +0100

    Struttura main

--- src/assets/components/Main.jsx
+++ src/assets/components/Main.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import languages from "../data/languages"
 
 const Main = () => {
     return (
-        <div>Main</div>
+
+        <div className="container">
+            <div className="sezioneBtn text-center">
+                {languages.map((linguaggi) => {
+                    return (
+                        <button className="mx-5 p-2 rounded-3" key={linguaggi.id} >{linguaggi.title}</button>
+                    )
+                })}
+            </div>
+            <div className="container sezioneTesto">
+                <h2 className="p-3">w</h2>
+                <p className="p-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum consequuntur vel ipsum optio laboriosam deserunt, fugiat deleniti? Aut sit culpa cum ea fugiat, sed placeat libero earum recusandae repudiandae voluptatem.</p>
+
+            </div>
+        </div>
     )
 }
 
